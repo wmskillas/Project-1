659289ceb6b887403bea09ee22c254bc068d1335
Carrington <mcarringtone@gmail.com>
2022-04-06 13:36:24 -0400

    commit

--- js/recipe.js
+++ js/recipe.js
@@ -1,22 +1,15 @@
-
 const options = {
-  method: "POST",
+  method: "GET",
   headers: {
-    "content-type": "application/json",
     "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     "X-RapidAPI-Key": "d0a8adab1emshcbaf23ad6f73065p1fa571jsneb56a6198206",
   },
-  body: '{\n  "ingredients": [\n    "eggs",\n    "bacon"\n  ],\n  "servings": 2,\n}',
 };
 
 fetch(
-  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/map",
+  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=burger&diet=vegetarian&excludeIngredients=coconut&intolerances=egg%2C%20gluten&number=10&offset=0&type=main%20course",
   options
 )
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
-
-var APIKey = '4f3c4ac653654ea0a6a8bcc5050be06e'
-
-// API Documentation page: https://spoonacular.com/food-api/docs
